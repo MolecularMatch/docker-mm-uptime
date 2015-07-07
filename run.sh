1c2c919be16d3da24f3d0729663973f8aa53c623
Ryan Smith <smithrp@gmail.com>
2015-07-07 12:19:59 -0700

    initial commit for uptime checking

---
+++ run.sh
@@ -0,0 +1,17 @@
+cd /tmp
+
+npm install
+
+#Pull down certs from etcd server and write them to /etc/nginx/
+nodejs get_certs.js
+
+cd /tmp/uptime
+
+#Start by setting up the configuration file with environment variables
+sed -i "s/{username}/$USERNAME/" /tmp/uptime/config/production.yaml
+sed -i "s/{password}/$PASSWORD/" /tmp/uptime/config/production.yaml
+sed -i "s/{emailUsername}/$EMAIL_USERNAME/" /tmp/uptime/config/production.yaml
+sed -i "s/{emailPassword}/$EMAIL_PASSWORD/" /tmp/uptime/config/production.yaml
+
+#Finally run the application
+node monitor & node app
